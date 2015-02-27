df1ad418f8c13b0f50f6f5be14dcd33aeb76690b
ewen <ewen.grosjean@free.fr>
2015-02-27 16:07:19 +0100

    game of life

--- src/cljs/hackday/game_of_life.cljs
+++ src/cljs/hackday/game_of_life.cljs
@@ -4,21 +4,55 @@
 
 (def param 10)
 
+
+
+(def old-game #js ["dead" "dead" "dead" "alive" "dead" "dead" "alive" "dead" "alive" "dead" "alive" "alive" "dead" "alive" "alive" "dead" "dead" "dead" "alive" "alive" "alive" "dead" "alive" "alive" "alive" "alive" "dead" "dead" "dead" "alive" "dead" "dead" "alive" "alive" "dead" "dead" "dead" "dead" "alive" "dead" "dead" "dead" "dead" "alive" "alive" "dead" "alive" "dead" "dead" "alive" "alive" "dead" "dead" "dead" "alive" "dead" "alive" "dead" "alive" "alive" "alive" "dead" "dead" "dead" "alive" "alive" "alive" "dead" "alive" "dead" "dead" "alive" "alive" "dead" "dead" "alive" "alive" "alive" "dead" "dead" "dead" "dead" "dead" "alive" "alive" "alive" "dead" "dead" "dead" "dead" "dead" "alive" "dead" "dead" "dead" "dead" "alive" "dead" "dead" "dead"])
+(def new-game #js [])
+
 (defn game->string [game]
   (->> (js->clj game)
        (partition 10)
        (interpose "\n")
        (apply str)))
 
-(defn init-game [game]
-  (let [init-indexes (take 60 (repeatedly (fn [] (rand-int 100))))
-        init-values (vec (interpose :alive init-indexes))
-        init-values (conj init-values :alive)]
-    init-game (apply (partial assoc game) init-values)))
+
+(defn neighbours [game i]
+  #js [(aget game (- i (+ param 1)))
+   (aget game (- i param))
+   (aget game (- i (- param 1)))
+   (aget game (+ i 1))
+   (aget game (+ i (+ param 1)))
+   (aget game (+ i param))
+   (aget game (+ i (- param 1)))
+   (aget game (- i 1))])
+
+(defn dead-cell-step [neighbours]
+  (if (= 3 (-> (filter #(= "alive" %) neighbours)
+               count))
+    "alive"
+    "dead"))
+
+(defn alive-cell-step [neighbours]
+  (let [alive-neighbour-nb (-> (filter #(= "alive" %) neighbours)
+                               count)]
+    (cond
+      (< alive-neighbour-nb 2) "dead"
+      (= alive-neighbour-nb 2) "alive"
+      (= alive-neighbour-nb 3) "alive"
+      (> alive-neighbour-nb 3) "dead")))
+
+(defn cell-step [game cell index]
+  (let [neighbours (neighbours game index)]
+    (if (= "dead" cell)
+      (dead-cell-step neighbours)
+      (alive-cell-step neighbours))))
 
 
-(def old-game nil)
-(def new-game nil)
+
+
+#_(.log js/console (str (game->string old-game)))
+
+
 
 
 (comment
